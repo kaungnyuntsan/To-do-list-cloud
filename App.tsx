187c93301b1208b71d3ecab6b96bc7de299a4b3e
Kaung-Nyunt-San <71218665+Kaung-Nyunt-San@users.noreply.github.com>
2024-03-09 22:32:47 +0630

    login screen draft

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import { HomeScreen } from "./screens/HomeScreen";
+import { LoginScreen } from "./screens/LoginScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
@@ -8,6 +9,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,10 +1,36 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export const LoginScreen = () => {
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}> Login Screen</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="email"
+        value={loginEmail}
+        onChangeText={setLoginEmail}
+        autoCapitalize="none"
+        keyboardType="email-address"
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="password"
+        value={loginPassword}
+        onChangeText={setLoginPassword}
+        secureTextEntry
+      />
+      <Button title="log in" onPress={() => {}} />
+      <Text style={{ textAlign: "center", fontSize: 18 }}>
+        {" "}
+        Forgot password?{" "}
+      </Text>
+      <Button title="create new account" onPress={() => {}} />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,10 +40,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: 50,
+    // alignItems: "center",
+    // justifyContent: "center",
   },
   text: {
     fontSize: 20,
+  },
+  input: {
+    height: 45,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
